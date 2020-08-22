37009beeb9e579b26a8fb269013c5150a0d61943
dvdmrn <dgm.marino@gmail.com>
2020-08-22 02:47:15 -0400

    eyebrows and multi networking

--- public/js/anim.js
+++ public/js/anim.js
@@ -6,9 +6,12 @@ fps = 15;
 
 var speed = 1;
 var _PADDING = 10;
+let circles = [];
+
+
 
 class Circle{
-	constructor(x,y,initX,initY,speed,rad,color,mouthDistance){
+	constructor(x,y,initX,initY,speed,rad,color, mouthDistance, id){
 	    this.x = x;
 	    this.y = y;
 	    this.initY = initY;
@@ -22,6 +25,9 @@ class Circle{
 	    this.mouthDistance = mouthDistance;
 	    this.localX = 0
 	    this.localY = 0
+	    this.eyeDist = 1
+	    this.radGain = 0.7;
+	    this.id = id;
 	}
     // Draw circle function
     
@@ -54,12 +60,13 @@ class Circle{
         this.x = 0.80*this.x + 0.20*localXMovement
         this.y = (0.80*this.y + 0.20*(localYMovement))+sinMovement
 
+        this.rad = 0.90*this.rad + 0.10*(this.eyeDist*this.radGain)
+
     };
 }
+var circle = new Circle(canvas.width/2,0,canvas.width/2,canvas.height,0.005,10,"#f7a5f5",1,"69");
 
 
-// #f7a5f5
-let circle = new Circle(canvas.width/2,0,canvas.width/2,canvas.height,0.005,10,"#f7a5f5",1)
 function draw() {
     ctx.clearRect(0,0,canvas.width, canvas.height);
     ctx.fillStyle = "white";
@@ -78,7 +85,28 @@ function draw() {
 draw()
 
 
+// TODO: emit to all clients or the viewer only. Assume this is the viewer? See notebook for more info
+socket.on('addClient',(client)=>{
+	let circle = new Circle(canvas.width/2,0,canvas.width/2,canvas.height,0.005,10,"#f7a5f5",1,client);
+	circles.push(circle);
+	console.log(`user ${client} just joined!`,circles);
 
+})
+
+socket.on('removeClient',(client)=>{
+	// let circle = new Circle(canvas.width/2,0,canvas.width/2,canvas.height,0.005,10,"#f7a5f5",1,client.id);
+	// circles.push(circle);
+      
+      for(i=0;i<circles.length;i++){
+      	if(circles[i].id==client.id){
+      		allClients.splice(i, 1);
+			console.log(`user ${client.id} just left! Goodbye!`,circles);
+      		return	
+      	}
+      }
+
+
+})
 socket.on('pos',(data)=>{
 	circle.mouthDistance = Math.max(mapRange(data.dist, 1200, 2000, 1,30),0);
 	// circle.speed = mapRange(data.dist, 800, 2000, 0,0.01);
@@ -86,6 +114,7 @@ socket.on('pos',(data)=>{
 		circle.localX = data.globalPos._x;
 		circle.localY = data.globalPos._y;
 	}
+	circle.eyeDist = data.eyeDist;
 	// console.log(circle.localX)
 	// console.log(data.globalPos);
 	// console.log(data.dist-1000);
